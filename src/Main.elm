4bafe5748a30754c0637da862d6725231a7c07a3
Eloy Robillard <eloy.robillard@gmail.com>
2025-11-17 16:38:58 +0100

    Edit answer

--- src/Main.elm
+++ src/Main.elm
@@ -378,18 +378,76 @@ quizView model =
 
         answerInner =
             if model.editingAnswer then
-                textarea
-                    [ class "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
-                    , attribute "data-slot" "textarea"
-                    , placeholder "Type the question here."
-                    , Events.onInput ChangeFreeTextAnswer
-                    , Html.Attributes.value model.freeTextAnswer
+                div [ Html.Attributes.class "flex flex-1 flex-col gap-1 [&+[data-slot=item-content]]:flex-none", attribute "data-slot" "item-content" ]
+                    [ div [ Html.Attributes.class "flex w-fit items-center gap-2 text-sm leading-snug font-medium", attribute "data-slot" "item-title" ]
+                        [ text "Answer" ]
+                    , textarea
+                        [ class "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                        , attribute "data-slot" "textarea"
+                        , placeholder "Type the question here."
+                        , Events.onInput ChangeFreeTextAnswer
+                        , Html.Attributes.value model.freeTextAnswer
+                        ]
+                        [ text model.freeTextAnswer ]
                     ]
-                    [ text model.freeTextAnswer ]
 
             else
-                p [ Html.Attributes.class "text-muted-foreground line-clamp-2 text-sm leading-normal font-normal text-balance [&>a:hover]:text-primary [&>a]:underline [&>a]:underline-offset-4", attribute "data-slot" "item-description" ]
-                    [ text answer ]
+                div [ class "w-full border-b last:border-b-0", attribute "data-orientation" "vertical", attribute "data-slot" "accordion-item", attribute "data-state" accordionState ]
+                    [ h3
+                        [ class "flex"
+                        , attribute "data-orientation" "vertical"
+                        , attribute "data-state"
+                            accordionState
+                        ]
+                        [ button
+                            [ attribute "aria-controls" "radix-_r_5b_"
+                            , attribute "aria-expanded"
+                                (if model.showAnswer then
+                                    "true"
+
+                                 else
+                                    "false"
+                                )
+                            , class "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180"
+                            , attribute "data-orientation" "vertical"
+                            , attribute "data-radix-collection-item" ""
+                            , attribute "data-slot" "accordion-trigger"
+                            , attribute "data-state" accordionState
+                            , id "radix-_r_5a_"
+                            , type_ "button"
+                            , Events.onClick ShowAnswer
+                            ]
+                            [ text "Answer"
+                            , svg [ class "lucide lucide-chevron-down text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200", fill "none", attribute "height" "24", attribute "stroke" "currentColor", attribute "stroke-linecap" "round", attribute "stroke-linejoin" "round", attribute "stroke-width" "2", viewBox "0 0 24 24", attribute "width" "24", attribute "xmlns" "http://www.w3.org/2000/svg" ]
+                                [ path [ d "m6 9 6 6 6-6" ]
+                                    []
+                                ]
+                            ]
+                        ]
+                    , div
+                        ([ attribute "aria-labelledby" "radix-_r_5a_", class "data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm", attribute "data-orientation" "vertical", attribute "data-slot" "accordion-content", attribute "data-state" accordionState, id "radix-_r_5b_", attribute "role" "region", attribute "style" "--radix-accordion-content-height: var(--radix-collapsible-content-height); --radix-accordion-content-width: var(--radix-collapsible-content-width); --radix-collapsible-content-height: 132px; --radix-collapsible-content-width: 472.3666687011719px;" ]
+                            ++ (if model.showAnswer then
+                                    []
+
+                                else
+                                    [ attribute "hidden" "" ]
+                               )
+                        )
+                        [ div [ class "pt-0 pb-4 flex flex-col gap-4 text-balance" ]
+                            [ p []
+                                [ text answer ]
+                            ]
+                        ]
+                    ]
+
+        answerButton =
+            if model.editingAnswer then
+                button [ Html.Attributes.class "inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5", attribute "data-slot" "button", Events.onClick SaveAnswer ]
+                    [ text "Save answer" ]
+
+            else
+                button [ Html.Attributes.class "inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5", attribute "data-slot" "button", Events.onClick EditAnswer ]
+                    [ text "Edit answer" ]
     in
     div [ Html.Attributes.class "flex w-full flex-col" ]
         [ div [ Html.Attributes.class "preview flex w-full justify-center data-[align=center]:items-center data-[align=end]:items-end data-[align=start]:items-start p-10", attribute "data-align" "center" ]
@@ -419,67 +477,22 @@ quizView model =
         , div [ Html.Attributes.class "preview flex w-full justify-center data-[align=center]:items-center data-[align=end]:items-end data-[align=start]:items-start p-10", attribute "data-align" "center" ]
             [ div
                 [ Html.Attributes.class "flex w-full max-w-md flex-col gap-6" ]
-                [ div [ Html.Attributes.class "group/item flex items-center border text-sm rounded-md transition-colors [a]:hover:bg-accent/50 [a]:transition-colors duration-100 flex-wrap outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] border-border p-4 gap-4", attribute "data-size" "default", attribute "data-slot" "item", attribute "data-variant" "outline" ]
-                    [ div [ Html.Attributes.class "flex flex-1 flex-col gap-1 [&+[data-slot=item-content]]:flex-none", attribute "data-slot" "item-content" ]
-                        [ div [ Html.Attributes.class "flex w-fit items-center gap-2 text-sm leading-snug font-medium", attribute "data-slot" "item-title" ]
-                            [ text "Question" ]
-                        , questionInner
+                [ div [ Html.Attributes.class "flex" ]
+                    [ div [ Html.Attributes.class "group/item flex items-center border text-sm rounded-md transition-colors [a]:hover:bg-accent/50 [a]:transition-colors duration-100 flex-wrap outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] border-border p-4 gap-4", attribute "data-size" "default", attribute "data-slot" "item", attribute "data-variant" "outline" ]
+                        [ div [ Html.Attributes.class "flex flex-1 flex-col gap-1 [&+[data-slot=item-content]]:flex-none", attribute "data-slot" "item-content" ]
+                            [ div [ Html.Attributes.class "flex w-fit items-center gap-2 text-sm leading-snug font-medium", attribute "data-slot" "item-title" ]
+                                [ text "Question" ]
+                            , questionInner
+                            ]
                         ]
                     , div [ Html.Attributes.class "flex items-center gap-2", attribute "data-slot" "item-actions" ]
                         [ questionButton ]
                     ]
-                , a [ Html.Attributes.class "group/item flex items-center border text-sm rounded-md transition-colors [a]:hover:bg-accent/50 [a]:transition-colors duration-100 flex-wrap outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] border-border py-3 px-4 gap-2.5", attribute "data-size" "sm", attribute "data-slot" "item", attribute "data-variant" "outline", href "#" ]
-                    [ div [ class "w-full border-b last:border-b-0", attribute "data-orientation" "vertical", attribute "data-slot" "accordion-item", attribute "data-state" accordionState ]
-                        [ h3
-                            [ class "flex"
-                            , attribute "data-orientation" "vertical"
-                            , attribute "data-state"
-                                accordionState
-                            ]
-                            [ button
-                                [ attribute "aria-controls" "radix-_r_5b_"
-                                , attribute "aria-expanded"
-                                    (if model.showAnswer then
-                                        "true"
-
-                                     else
-                                        "false"
-                                    )
-                                , class "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180"
-                                , attribute "data-orientation" "vertical"
-                                , attribute "data-radix-collection-item" ""
-                                , attribute "data-slot" "accordion-trigger"
-                                , attribute "data-state" accordionState
-                                , id "radix-_r_5a_"
-                                , type_ "button"
-                                , Events.onClick ShowAnswer
-                                ]
-                                [ text "Answer"
-                                , svg [ class "lucide lucide-chevron-down text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200", fill "none", attribute "height" "24", attribute "stroke" "currentColor", attribute "stroke-linecap" "round", attribute "stroke-linejoin" "round", attribute "stroke-width" "2", viewBox "0 0 24 24", attribute "width" "24", attribute "xmlns" "http://www.w3.org/2000/svg" ]
-                                    [ path [ d "m6 9 6 6 6-6" ]
-                                        []
-                                    ]
-                                ]
-                            ]
-                        , div
-                            ([ attribute "aria-labelledby" "radix-_r_5a_", class "data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm", attribute "data-orientation" "vertical", attribute "data-slot" "accordion-content", attribute "data-state" accordionState, id "radix-_r_5b_", attribute "role" "region", attribute "style" "--radix-accordion-content-height: var(--radix-collapsible-content-height); --radix-accordion-content-width: var(--radix-collapsible-content-width); --radix-collapsible-content-height: 132px; --radix-collapsible-content-width: 472.3666687011719px;" ]
-                                ++ (if model.showAnswer then
-                                        []
-
-                                    else
-                                        [ attribute "hidden" "" ]
-                                   )
-                            )
-                            [ div [ class "pt-0 pb-4 flex flex-col gap-4 text-balance" ]
-                                [ p []
-                                    [ text answer ]
-                                ]
-                            ]
-                        ]
+                , div [ Html.Attributes.class "flex" ]
+                    [ div [ Html.Attributes.class "group/item flex items-center border text-sm rounded-md transition-colors [a]:hover:bg-accent/50 [a]:transition-colors duration-100 flex-wrap outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] border-border py-3 px-4 gap-2.5", attribute "data-size" "sm", attribute "data-slot" "item", attribute "data-variant" "outline" ]
+                        [ answerInner ]
                     , div [ Html.Attributes.class "flex items-center gap-2", attribute "data-slot" "item-actions" ]
-                        [ button [ Html.Attributes.class "inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5", attribute "data-slot" "button", Events.onClick EditAnswer ]
-                            [ text "Edit answer" ]
-                        ]
+                        [ answerButton ]
                     ]
                 ]
             ]
