b864b5f319d522c29319134dce8c88bc707986e0
Eloy Robillard <eloy.robillard@gmail.com>
2025-11-25 16:17:37 +0100

    Update styles.css with styles from shadcn website

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import "./index.css";
+import "./styles.css";
 import { Elm } from "./Main.elm";
 
 // Mount "Main" Browser.{element,document} on #root
 Elm.Main.init({
-	node: document.querySelector("main"),
-	flags: "Initial Message",
+  node: document.querySelector("main"),
+  flags: "Initial Message",
 });
